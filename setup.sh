d5ad0234150a5a049b8b37f58b82b8d696101d6a
Quang Kieu <quang.p.kieu@gmail.com>
2024-11-06 02:36:42 -0500

    Skip confirmation for installing ROS

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 # Constants to be updated
 ROS_VERSION=jazzy
@@ -23,7 +24,7 @@ sudo apt install -y curl
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 sudo apt update
-sudo apt install ros-$ROS_VERSION-ros-base python3-rosdep python3-colcon-common-extensions python3-pip
+sudo apt install -y ros-$ROS_VERSION-ros-base python3-rosdep python3-colcon-common-extensions python3-pip
 sudo rosdep init
 rosdep update
 echo "source /opt/ros/$ROS_VERSION/setup.bash" >> ~/.bashrc
