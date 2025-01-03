8f576683b972fe7881f1f7b442c243e28e3603f6
qkieu <quang.p.kieu@gmail.com>
2025-01-03 11:36:42 -0500

    Add SSID as network information

--- files/pollmemaybe.sh
+++ files/pollmemaybe.sh
@@ -2,4 +2,5 @@
 
 team="$(hostname)"
 ip="$(hostname -I | grep -oh -E -i -w '\b((10)|(18))[^ ]+')"
-curl "http://maslab.mit.edu/pollmemaybe/?team=${team}&ip=${ip}">/dev/null
+ssid="$(iw dev wlan0 info | grep ssid | awk '{print $2}')"
+curl "http://maslab.mit.edu/pollmemaybe/?team=${team}&ip=${ip}&ssid=${ssid}">/dev/null
