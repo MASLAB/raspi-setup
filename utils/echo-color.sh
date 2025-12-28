07307d3dcd6316f3011535e80b8cba5b0ac81fb8
Team 0 <team0@team0pi>
2025-12-28 05:51:32 -0500

    More colors

--- utils/echo-color.sh
+++ utils/echo-color.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
 # Echo colors
+ECHO_BLACK='\033[1;30m'
+ECHO_RED='\033[1;31m'
+ECHO_GREEN='\033[1;32m'
+ECHO_ORANGE='\033[1;33m'
 ECHO_BLUE='\033[1;34m'
 ECHO_PURPLE='\033[1;35m'
+ECHO_CYAN='\033[1;36m'
 ECHO_GRAY='\033[1;37m'
 ECHO_CLEAR='\033[0m' # No Color
 
